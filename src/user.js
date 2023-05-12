7e1b9456fcdc4347392db9a6753111a96a32f369
rikusrocks <rikusrocks@gmail.com>
2023-05-12 17:16:50 +1000

    connect db

--- src/user.js
+++ src/user.js
@@ -1,3 +1,23 @@
+import mongoose from "mongoose";
+import uuid from 'react-uuid';
+
 export const listUser = (req, res) => {
     res.status(201).send("get user list")
+}
+
+export const regitser = (req, res) => {
+    console.log(req.body)
+    const userSchema = new mongoose.Schema({
+        email : req.body.email,
+        id: uuid(),
+        project : [ {
+            task: {
+                title,
+                todo,
+                doing,
+                done
+            }
+        }]
+    })
+    res.status(201).send("register user")
 }
